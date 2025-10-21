f6fd6af33a77960351b288535b21c61b40ee0967
Pablo <seuemail@exemplo.com>
2025-10-21 16:01:41 -0300

    criação de produto

--- src/Produto.java
+++ src/Produto.java
@@ -1,2 +1,61 @@
 public class Produto {
+    private final int id;
+    private String nome;
+    private String codigoBarras;
+    private double preco;
+    private double custoMedio;
+    private int estoque;
+
+    public Produto(int id, String nome, double preco, String codigoBarras, double custoMedio, int estoque) {
+        this.id = id;
+        this.nome = nome;
+        this.preco = preco;
+        this.codigoBarras = codigoBarras;
+        this.custoMedio = custoMedio;
+        this.estoque = estoque;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCodigoBarras() {
+        return codigoBarras;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public double getCustoMedio() {
+        return custoMedio;
+    }
+
+    public int getEstoque() {
+        return estoque;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setCodigoBarras(String codigoBarras) {
+        this.codigoBarras = codigoBarras;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public void setCustoMedio(double custoMedio) {
+        this.custoMedio = custoMedio;
+    }
+
+    public void setEstoque(int estoque) {
+        this.estoque = estoque;
+    }
 }
