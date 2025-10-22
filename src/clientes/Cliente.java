ff60f048e9fd2e6bfa5e830ae454a7bc1bb838dd
JOÃO PEDRO RAMOS INÁCIO <joao_pr_inacio@estudante.sesisenai.org.br>
2025-10-21 21:42:51 -0300

    Criado todas as classes

--- src/clientes/Cliente.java
+++ src/clientes/Cliente.java
@@ -1,4 +1,15 @@
 package clientes;
 
 public class Cliente {
+    private int id;
+    private String nome;
+    private String telefone;
+    priate Categoria categoria;
+
+    public Cliente(int id, String nome, String telefone, priate categoria) {
+        this.id = id;
+        this.nome = nome;
+        this.telefone = telefone;
+        Categoria = categoria;
+    }
 }
