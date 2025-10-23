cc2513e9e2f75e278f078145f83fc855765350be
Pablo <pabloantoni052007@gmail.com>
2025-10-23 08:34:31 -0300

    ENUM FEITO

--- src/clientes/Categoria.java
+++ src/clientes/Categoria.java
@@ -1,4 +1,18 @@
 package clientes;
 
 public enum Categoria {
+    BRONZE(0.02),
+    PRATA(0.05),
+    OURO(0.10),
+    PLATINA(0.15);
+
+    private final double percentualDesconto;
+
+    Categoria(double percentualDesconto) {
+        this.percentualDesconto = percentualDesconto;
+    }
+
+    public double getPercentualDesconto() {
+        return percentualDesconto;
+    }
 }
